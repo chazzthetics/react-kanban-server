f5313269f41ea74ab1ff8282549936346f3aa0a1
Chazz Lee <l.chazz00@gmail.com>
2020-04-06 19:50:55 -0400

    create boards migration

---
+++ database/migrations/2020_04_06_234609_create_boards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('title', 30);
+            $table->string('color', 15);
+            $table->string('slug');
+            $table->boolean('is_current')->default(true);
+            $table->boolean('is_starred')->default(false);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
